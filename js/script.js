1db0185ded5356bed48d0b2e3e310569cc8ae192
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-06-21 14:44:38 +0200

    const date added

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const sumbmitElement = document.querySelector(".js-submit")
 const resultElement = document.querySelector(".js-result");
 const formElement = document.querySelector(".js-form");
 
+
 const euro = 4.55;
 
 formElement.addEventListener("submit", (event) => {
@@ -17,5 +18,22 @@ sumbmitElement.addEventListener("click", () => {
     resultElement.innerText = result.toFixed(2) +   "  €";
 });
 
+
 }
 
+{
+    const ShowDateElement = document.querySelector(".js-data");
+       
+            const ShowDate = () => {
+                const newDate = new Date();
+                const todayDate = newDate.toLocaleDateString(  
+                    {
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric",
+                        weekday: "long",                                   
+                    });               
+
+                ShowDateElement.innerText = ShowDate.formElement {todayDate} {newDate.toLocaleDateString()
+                  
+                )};
