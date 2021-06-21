947c0ead3f33111b8d88bc7a0e45fba5ce90476d
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-06-21 15:47:22 +0200

    const of Element added

--- js/script.js
+++ js/script.js
@@ -1,36 +1,43 @@
 {
     const inputElement = document.querySelector(".js-input");
-const sumbmitElement = document.querySelector(".js-submit")
-const resultElement = document.querySelector(".js-result");
-const formElement = document.querySelector(".js-form");
+    const sumbmitElement = document.querySelector(".js-submit")
+    const resultElement = document.querySelector(".js-result");
+    const formElement = document.querySelector(".js-form");
 
+    const euro = 4.55;
 
-const euro = 4.55;
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
+    });
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-});
+    sumbmitElement.addEventListener("click", () => {
+        const result = inputElement.value / euro;
 
-
-sumbmitElement.addEventListener("click", () => {
-    const result = inputElement.value / euro;
-
-    resultElement.innerText = result.toFixed(2) +   "  €";
-});
-
+        resultElement.innerText = result.toFixed(2) + "  €";
+    });
 
 }
 
 {
     const ShowDateElement = document.querySelector(".js-data");
-       
-            const ShowDate = () => {
-                const newDate = new Date();
-                const todayDate = newDate.toLocaleDateString(  
-                    {
-                        year: "numeric",
-                        month: "long",
-                        day: "numeric",
-                        weekday: "long",                                   
-                    });               
-                }}
+
+    ShowDateElement = [todayDate, ShowDateElement]
+
+
+    const ShowDateElement = () => {
+        const newDate = new Date();
+        const todayDate = newDate.toLocaleDateString(
+            {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+                weekday: "long",
+            });
+    };
+
+    return (
+        <p className="date">
+            Dzisiejsza data: todayDate();
+        </p>
+    );
+}
