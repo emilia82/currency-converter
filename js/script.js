053f7947916bdc803103e2a8fd49c0f3a3e0810b
Emilia Warakomska <emilia.warakomska@gmail.com>
2021-07-14 14:51:52 +0200

    typo corrected

--- js/script.js
+++ js/script.js
@@ -1,54 +1,54 @@
 {
-    
+
     const calculateResult = (currency, amount) => {
-        
+
         const rateEUR = 4.5649;
         const rateUSD = 3.7855;
-        const rateGB = 5.2579; 
-        const rateCHF =  4.1148;
+        const rateGB = 5.2579;
+        const rateCHF = 4.1148;
 
-    switch (currency) {
-        case "EUR": 
-        return amount / rateEUR;
+        switch (currency) {
+            case "EUR":
+                return amount / rateEUR;
 
-        case "USD":
-        return amount / rateUSD;
+            case "USD":
+                return amount / rateUSD;
 
-        case "GB": 
-        return amount / rateGB; 
-        
-        case "CHF":
-        return amount / rateCHF;
-    }
+            case "GB":
+                return amount / rateGB;
+
+            case "CHF":
+                return amount / rateCHF;
+        }
     };
-    
-   
-    const updateResultText = ( currency, amount, result ) => {
-        const resultElement = document.querySelector(".js-result"); 
 
-        resultElement.innerText = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;   
+
+    const updateResultText = (currency, amount, result) => {
+        const resultElement = document.querySelector(".js-result");
+
+        resultElement.innerText = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
 
     }
 
-const onFormSubmit = (event) => {
-    event.preventDefault();
+    const onFormSubmit = (event) => {
+        event.preventDefault();
 
-    const amountElement = document.querySelector(".js-amount");
-    const currencyElement = document.querySelector(".js-currency");
+        const amountElement = document.querySelector(".js-amount");
+        const currencyElement = document.querySelector(".js-currency");
 
-    const amount = +amountElement.value; 
-    const currency = currencyElement.value; 
+        const amount = +amountElement.value;
+        const currency = currencyElement.value;
 
-    const result = calculateResult(curency, amount);
-    updateResultText(amount, currency, result);
+        const result = calculateResult(curency, amount);
+        updateResultText(amount, currency, result);
 
-};
+    };
 
-const init = () => {
-    const formElement = document.querySelector(".js-form");
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
 
-    formElement.addEventListener("submit", onFormSubmit);
-    }; 
+        formElement.addEventListener("submit", onFormSubmit);
+    };
 
     init();
 };
@@ -57,7 +57,7 @@ const init = () => {
 {
     const clock = document.querySelector(".js-clock");
 
-    ShowDateElement = [todayDate, clock]
+    ShowDateElement = (todayDate, clock);
 
 
     const ShowDateElement = () => {
@@ -69,11 +69,13 @@ const init = () => {
                 day: "numeric",
                 weekday: "long",
             });
-    };
+    
 
     return (
         <p class="date">
             Dzisiejsza data: todayDate();
         </p>
     );
+
+    }
 };
